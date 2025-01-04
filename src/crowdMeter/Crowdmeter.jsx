f4b6c209a67d9aebc788155e57f3634490b982c8
luisgcode <luisgcode@gmail.com>
2025-01-03 22:20:01 -0500

    Starting the component design

--- src/crowdMeter/Crowdmeter.jsx
+++ src/crowdMeter/Crowdmeter.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./Crowdmeter";
 
 const Crowdmeter = () => {
-  return <div className="card">Hello there, testing Github</div>;
+  return (
+    <div className="container">
+      <h1 className="title text-titleColor uppercase font-bold">
+        Schedules & hours
+      </h1>
+    </div>
+  );
 };
 
 export default Crowdmeter;
