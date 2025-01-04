a52f17d1e492775f2901a4ea3bbad19da6bed4f6
luisgcode <luisgcode@gmail.com>
2025-01-04 12:49:36 -0500

    Data emulator design done

--- src/crowdMeter/components/Simulator.jsx
+++ src/crowdMeter/components/Simulator.jsx
@@ -3,21 +3,37 @@ import "./Simulator.css";
 
 const Simulator = () => {
   return (
-    <div className="flex justify-center flex-col items-center  ">
-      <div className="row">
-        <select name="" id="select_day">
-          <option value="Monday">Monday</option>
-          <option value="tuesday">Tuesday</option>
-          <option value="Monday">Wednesday</option>
-          <option value="Monday">Monday</option>
-          <option value="Monday">Monday</option>
-          <option value="Monday">Monday</option>
-          <option value="sunday">Sunday</option>
-        </select>
+    <div className="text-center">
+      <span className="text-sm text-yellow-300">* API data emulator</span>
+      <div className="dashboard flex align-center justify-center gap-10 mt-5">
+        <div className="dashboard-block">
+          <select
+            className="rounded-lg bg-gray-800 p-4"
+            name=""
+            id="select_day"
+          >
+            <option value="monday">Monday</option>
+            <option value="tuesday">Tuesday</option>
+            <option value="wednesday">Wednesday</option>
+            <option value="thursday">Thursday</option>
+            <option value="friday">Friday</option>
+            <option value="saturday">Saturday</option>
+            <option value="sunday">Sunday</option>
+          </select>
+        </div>
+        <div className="dashboard-block">
+          <input
+            className="p-4 rounded-lg bg-gray-800 min-w-[340px]"
+            type="number"
+            name="people"
+            id="people"
+            placeholder="Insert number of people here from 0 to 100"
+          />
+        </div>
+        <div className="dashboard-block">
+          <button className="p-4 rounded-lg bg-gray-800">Insert data</button>
+        </div>
       </div>
-      <span className="text-yellow-300 text-sm tracking-widest text-left block mt-5">
-        * Insert data to emulate API.
-      </span>
     </div>
   );
 };
