6efa519c9e95fa36046629a2cb7b490eba5bb30f
luisgcode <luisgcode@gmail.com>
2025-01-03 23:46:17 -0500

    Work in progress

--- src/crowdMeter/Crowdmeter.jsx
+++ src/crowdMeter/Crowdmeter.jsx
@@ -13,12 +13,23 @@ const Crowdmeter = () => {
         </div>
         <div className="schedule-content flex gap-10  ">
           <div className="hours w-[300px]">
-            <h2 className="font-bold uppercase border-2 border-white">hours</h2>
+            <h2 className="font-bold uppercase mb-10">hours</h2>
+            <div className="hours-block flex flex-col gap-1 text-left  ">
+              <span>Monday: 5:00 AM - 12:00 AM</span>
+              <span>Tuesday: 24 HRS</span>
+              <span>Wednesday: 24 HRS</span>
+              <span>Thursday: 5:00 AM - 12:00 AM</span>
+              <span>Friday: 7:00 AM - 09:00 PM</span>
+              <span>Saturday: 5:00 AM - 12:00</span>
+              <span>Sunday: 5:00 AM - 12:00</span>
+            </div>
           </div>
           <div className="crowd w-[700px]">
-            <h2 className="font-bold uppercase border-2 border-white">
-              daily crowd
-            </h2>
+            <h2 className="font-bold uppercase mb-10">daily crowd</h2>
+            <div className="crowd-block  "></div>
+            <span className="text-yellow-300 text-sm tracking-widest ">
+              * Info Collected from the last 30 days
+            </span>
           </div>
         </div>
       </div>
